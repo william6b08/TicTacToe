08b4c80efeac911cf9d0d3ad2e2202139cb6fb37
william6b08 <william6b08@yahoo.com.hk>
2022-09-16 13:56:56 +0800

    Added function to highlight patterns and restart button

--- script.js
+++
@@ -1,8 +0,0 @@
-const Obj1 = {gender: "Male", name: "Ob1"}
-const Obj2 = {gender: "Male", name: "Ob2"}
-const Obj3 = {gender: "Male", name: "Ob3"}
-const Obj4 = {gender: "Male", name: "Ob4"}
-result = Array.from(Obj1, (Obj1)=>Obj1.gender)
-console.log(result)
-
-
